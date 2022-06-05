d83b8633fd90656388709f82a80a13ef53850be7
Sh-Revo <shandov@i.ua>
2022-06-05 23:48:31 +0300

    create lights

---
+++ Lights/Assets/Scripts/Move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    public float speed;
+    private float moveHorizontal;
+    private float moveVertical;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rigidbody.AddForce(move * speed);
+    }
+
+}
